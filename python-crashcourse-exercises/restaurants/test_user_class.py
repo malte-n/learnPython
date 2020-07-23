18bd16d1c4fc6a20e5d454774a32cdd2a55af7dd
Malte Niepel <maltetorben.niepel@gmail.com>
2020-07-23 10:22:02 +0200

    adjusted using setUp method for class unit test

--- python-crashcourse-exercises/restaurants/test_user_class.py
+++ python-crashcourse-exercises/restaurants/test_user_class.py
@@ -3,10 +3,11 @@ from user_rights import User
 
 class UserTestCase(unittest.TestCase):
     """Tests for user rights"""
+    def setUp(self):
+        self.test_user = User(first_name="Jan", last_name="Kowalski")
 
     def test_user_rights_input(self):
-        test_user = User(first_name="Jan", last_name="Kowalski")
-        name_test_user = test_user.describe_user()
+        name_test_user = self.test_user.describe_user()
         self.assertEqual(name_test_user, 'Jan Kowalski')
 
 
