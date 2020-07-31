a757cc9e54d0f5815083e2dc5b61e4f0d30e3608
Malte Niepel <maltetorben.niepel@gmail.com>
2020-07-31 08:19:11 +0200

    try selenium

---
+++ test_selenium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome(
+    executable_path=r"/Users/malte.niepel/Desktop/mniepel/DataScience/chromedriver"
+)
+
+type(browser)
+
+browser.get("https://inventwithpython.com")
+
+try:
+    elem = browser.find_element_by_class_name("cover-thumb")
+    print("Found <%s> element with that class name!" % (elem.tag_name))
+except:
+    print("Was not able to find an element with that name.")
